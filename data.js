cd2df2535aab876327feb5f22759438822a6442d
bopi1829 <bopi1829@gmail.com>
2024-06-19 13:43:41 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,12 @@
+const phones = [
+  {
+    id: 1,
+    marque: 'apple',
+    model: 'iPhone 13',
+    description: 'téléphone avec vitre renforcée',
+    annee: '2021',
+    service: true,
+    couleur: '2018BA',
+    capacité: '256 Go',
+  },
+];
